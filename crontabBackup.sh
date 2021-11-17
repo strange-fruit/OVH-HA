cd6296d1cc4bf7653d4a259b548df54aaa4beb8c
@ncls-msr <80158890+strange-fruit@users.noreply.github.com>
2021-11-17 15:13:15 +0100

    Update crontabBackup.sh

--- crontabBackup.sh
+++ crontabBackup.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#user save created only for this task
+#OVH is the root directiory in archive.tar
+# exemple : OVH/connexion.php
+
+#script exectued after VPS sent archive via SCP
+
+cd /home/save/
+tar -xvf archive.tar
+cp OVH/* /var/www/html/
+rm OVH -r 
+rm archive.tar
 
